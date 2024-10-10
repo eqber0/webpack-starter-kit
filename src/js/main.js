7d414b2a0e2c002db0998bbba6332d2a12756131
Bilgehan Yılmaz <eqber00@gmail.com>
2024-10-10 14:49:34 +0300

    stater kit preloader updated, cookie alert & 404 example page added. Some components and js files adjusted.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,93 @@
 // Imports
+import Headroom from "headroom.js"
 import components from "./component.js"
-components()
 
 // Functions
 var app = {
   load() {
+    components()
     console.log("load")
   },
   resized() {
     console.log("resized")
   },
+  preloaderFn() {
+    const preloader = document.querySelector(".preloader")
+    if (preloader) {
+      preloader.classList.add("hidden")
+      setTimeout(() => {
+        preloader.remove()
+      }, 400)
+    }
+  },
+  cookieAlertFn() {
+    const cookieAlert = document.querySelector(".js-cookie-alert")
+    const acceptBtn = document.querySelector(".js-cookie-accept")
+    const declineBtn = document.querySelector(".js-cookie-decline")
 
+    function getCookie(name) {
+      const value = `; ${document.cookie}`
+      const parts = value.split(`; ${name}=`)
+      if (parts.length === 2) return parts.pop().split(";").shift()
+      return null
+    }
+
+    function setCookie(name, value, days) {
+      const date = new Date()
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000) // Set expiration time in milliseconds
+      const expires = `expires=${date.toUTCString()}`
+      document.cookie = `${name}=${value}; ${expires}; path=/`
+    }
+
+    if (getCookie("cookieConsent") !== "true") {
+      setTimeout(() => {
+        cookieAlert.classList.remove("hidden")
+      }, 1500)
+    } else {
+      cookieAlert.remove()
+    }
+
+    function acceptCookies() {
+      setCookie("cookieConsent", "true", 30)
+      cookieAlert.classList.add("hidden")
+      setTimeout(() => {
+        cookieAlert.remove()
+      }, 350)
+    }
+    function declineCookies() {
+      cookieAlert.classList.add("hidden")
+      setTimeout(() => {
+        cookieAlert.remove()
+      }, 350)
+    }
+    acceptBtn.addEventListener("click", acceptCookies)
+    declineBtn.addEventListener("click", declineCookies)
+  },
+  headroomFn() {
+    const header = document.querySelector("header")
+    const options = {
+      offset: {
+        up: 100,
+        down: 50,
+      },
+      tolerance: {
+        up: 5,
+        down: 0,
+      },
+      classes: {
+        initial: "header",
+        pinned: "header--pinned",
+        unpinned: "header--unpinned",
+        top: "header--top",
+        notTop: "header--not-top",
+        bottom: "header--bottom",
+        notBottom: "header--not-bottom",
+        frozen: "header--frozen",
+      },
+    }
+    const headroom = new Headroom(header, options)
+    headroom.init()
+  },
   languageDropdownFn() {
     const headerLanguage = document.querySelector(".header__language")
     if (headerLanguage) {
@@ -85,6 +162,9 @@ var app = {
 
   init: function () {
     app.load()
+    app.preloaderFn()
+    app.cookieAlertFn()
+    app.headroomFn()
     app.languageDropdownFn()
     app.mobileMenuFn()
   },
